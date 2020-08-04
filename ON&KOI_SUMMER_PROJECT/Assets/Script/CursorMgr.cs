44aaecc01120d400e5d86ee77020e9d6fa0d1100
채영훈 <chaeyh04@gmail.com>
2020-08-04 17:46:28 +0900

    2020-08-04

---
+++ ON&KOI_SUMMER_PROJECT/Assets/Script/CursorMgr.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorMgr : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
